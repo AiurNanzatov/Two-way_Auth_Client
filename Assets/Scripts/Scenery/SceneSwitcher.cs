63e143aed0dc685bf101b13949a73089dd8b4367
AiurNanzatov <aznanzatov@mun.ca>
2021-09-23 14:10:04 -0230

    First commit

---
+++ Assets/Scripts/Scenery/SceneSwitcher.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+
+{
+    public void LoginPassMethod()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void DigitalCodeMethod()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void BacktoMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void CAPTCHASwitcher()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+}
